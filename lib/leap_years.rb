b32c0af54cee0d40ea35b358c324f8ebd5ae55ca
Kehinde Olofinmoyin <kehindeolofinmoyin@hotmail.com>
2021-01-13 16:20:39 +0000

    takes 1900 and returns false.

--- lib/leap_years.rb
+++ lib/leap_years.rb
@@ -1,3 +1,3 @@
 def leap_years(year)
-  true
+  year % 400 == 0 ? true : false
 end
--- spec/leap_years_spec.rb
+++ spec/leap_years_spec.rb
@@ -4,4 +4,8 @@ describe "#leap_years" do
   it "takes 1600 and returns true" do
     expect(leap_years(1600)).to eq true
   end
+
+   it "takes 1900 and returns false" do
+     expect(leap_years(1900)).to eq false
+   end
 end
